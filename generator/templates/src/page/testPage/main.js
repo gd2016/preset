8cb64928e42054d209c8fa7a55a4336a534895f6
guoding <guoding@ct108.com>
2018-11-16 14:48:44 +0800

    .

--- generator/templates/src/page/testPage/main.js
+++ generator/templates/src/page/testPage/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue';
 import router from '../../router/testPage.js';
 import VueRouter from 'vue-router';
 import '../../assets/scss/common.scss';
-import '../../filter/vue-filter'; 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 new Vue({
